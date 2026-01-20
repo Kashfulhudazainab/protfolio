deeb8fb5cfe65043d138c199dcfb6fef195259f1
kashful huda <kashfulhudazainab@gmail.com>
2026-01-19 23:39:29 -0700

    final

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI,{
-      dbName: 'portfolio_db'
+      dbName: 'protfolio_db'
     });
     console.log(`✅ Connected to Database: ${conn.connection.name}`);
     console.log(`✅ Host: ${conn.connection.host}`);
